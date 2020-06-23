78e99dba881c8a12793c40dbb8a6152ff6ad21bb
shubhashi88 <35379011+shubhashi88@users.noreply.github.com>
2020-06-23 15:53:58 +0530

    Update hosts.sh

--- hosts.sh
+++ hosts.sh
@@ -1,4 +1,2 @@
-@echo off
-echo 185.72.130.41 rewesports2019community.tccglobal.com> %temp%\temphosts.txt
-type C:\WINDOWS\system32\drivers\etc\hosts >> %temp%\temphosts.txt
-copy /Y %temp%\temphosts.txt C:\WINDOWS\system32\drivers\etc\hosts
+#!/bin/bash
+echo "185.72.130.41  rewesports2019community.tccglobal.com " >> /etc/hosts
