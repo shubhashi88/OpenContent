1c9a7c25e5b1f2d84f87586b7dea1a0fecba24a8
shubhashi88 <35379011+shubhashi88@users.noreply.github.com>
2020-06-26 12:38:31 +0530

    Update hosts.sh

--- hosts.sh
+++ hosts.sh
@@ -1,2 +1,6 @@
 #! /usr/local/bin/bash
-echo "185.72.130.41  rewesports2019community.tccglobal.com" >> /etc/hosts
+cp /etc/hosts /var/TMP/temphosts.txt
+echo "       185.72.130.41  rewesports2019community.tccglobal.com" >> /var/TMP/temphosts.txt
+cp  /var/TMP/temphosts.txt /etc/hosts
+
+
